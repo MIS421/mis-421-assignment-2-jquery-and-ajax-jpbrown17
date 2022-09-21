88a808c9d0ed2cc403b75dfda0c3016339098f77
John (Jack) Brown <jpbrown5@crimson.ua.edu>
2022-09-20 23:53:27 -0500

    Add project files.

---
+++ mis-421-assignment-2-jquery-and-ajax-jpbrown17-webapp/wwwroot/script.js
@@ -0,0 +1,36 @@
+var len;
+var results = '';
+
+function apiSearch() {
+  var params = {
+    "q": $("#query").val(),
+    "count": "50",
+    "offset": "0",
+    "mkt": "en-us"
+  };
+
+  $.ajax({
+      url: 'https://api.bing.microsoft.com/v7.0/search?' + $.param(params),
+      beforeSend: function (xhrObj) {
+          xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "f08dc24bf0754b8ab5c03bd104c9ed1b");
+      },
+      type: "GET",
+    })
+    .done(function (data) {
+      len = data.webPages.value.length;
+      for (i = 0; i < len; i++) {
+        results += "<p><a href='" + data.webPages.value[i].url + "'>" + data.webPages.value[i].name + "</a>: " + data.webPages.value[i].snippet + "</p>";
+      }
+
+      $('#searchResults').html(results);
+      $('#searchResults').dialog();
+    })
+    .fail(function () {
+      alert("error");
+    });
+}
+
+function changeImage() {
+    document.getElementById('body').style.background = url("https://images7.alphacoders.com/887/thumbbig-887467.webp")
+}
+
